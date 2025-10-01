031a029d97769c6abad9dd9098a4845b0a9ae07a
Sakshi Kumari <sakshirai887@gmail.com>
2025-10-01 15:00:38 +0530

    Create 1518_Water_Bottel.cpp

---
+++ DailyProblemSolution/1518_Water_Bottel.cpp
@@ -0,0 +1,21 @@
+problrm link- https://leetcode.com/problems/water-bottles/submissions/1787994769/?envType=daily-question&envId=2025-10-01
+
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int totalDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            int newBottles = emptyBottles / numExchange;
+            int remainingEmpty = emptyBottles % numExchange;
+            
+            totalDrunk += newBottles;
+            
+            emptyBottles = newBottles + remainingEmpty;
+        }
+        
+        return totalDrunk;
+    }
+};
