810d89019925d7eb98b7ebec361c5e9f7c033184
Sakshi Kumari <sakshirai887@gmail.com>
2026-02-24 16:34:35 +0530

    27. Remove Element

---
+++ Python/que4.py
@@ -0,0 +1,14 @@
+# problem link-https://leetcode.com/problems/remove-element/?envType=problem-list-v2&envId=array
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        if not nums:
+            return 0
+        
+        k=0
+        for i in range(0,len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k
+        
